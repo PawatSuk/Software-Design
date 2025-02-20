f1cc4792d401014fff5d3cb818e0b0e7246e7000
PawatSuk <109640442+PawatSuk@users.noreply.github.com>
2025-02-20 22:51:50 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("12345", "Explorer", "X100", 2024, "Holiday Motors", 55000.0);
+        vehicle.displayInfo();
+    }
+}
